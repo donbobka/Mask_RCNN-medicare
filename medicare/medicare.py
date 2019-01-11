e260b279ce3e889778915e4884d1d1389fa7709e
Vladimir Lyzo <vladimir@lyzo.me>
2019-01-11 16:48:51 +1100

    Add medicare

---
+++ medicare/medicare.py
@@ -0,0 +1,101 @@
+"""
+Mask R-CNN
+Configurations and data loading code for the synthetic Shapes dataset.
+This is a duplicate of the code in the noteobook train_shapes.ipynb for easy
+import into other notebooks, such as inspect_model.ipynb.
+
+Copyright (c) 2017 Matterport, Inc.
+Licensed under the MIT License (see LICENSE for details)
+Written by Waleed Abdulla
+"""
+
+import os
+import sys
+import math
+import random
+import numpy as np
+import cv2
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import utils
+
+
+class MedicareConfig(Config):
+    """Configuration for training on the toy shapes dataset.
+    Derives from the base Config class and overrides values specific
+    to the toy shapes dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "medicare"
+
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 8
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # background + 1 shapes
+
+    # Use a small epoch since the data is simple
+    STEPS_PER_EPOCH = 100
+
+    # use small validation steps since the epoch is small
+    VALIDATION_STEPS = 5
+
+class MedicareDataset(utils.Dataset):
+    def load_medicare(self, dataset_dir, subset):
+        """Load a subset of the medicare dataset.
+
+        dataset_dir: Root directory of the dataset
+        subset: Subset to load. Either the name of the sub-directory,
+                such as stage1_train, stage1_test, ...etc. or, one of:
+                * train: stage1_train excluding validation images
+                * val: validation images from VAL_IMAGE_IDS
+        """
+        # Add classes. We have one class.
+        # Naming the dataset nucleus, and the class nucleus
+        self.add_class("medicare", 1, "medicare")
+
+        dataset_dir = os.path.join(dataset_dir, subset, 'images')
+        image_ids = next(os.walk(dataset_dir))[2]
+
+        # Add images
+        for image_id in image_ids:
+            self.add_image(
+                "medicare",
+                image_id=image_id,
+                path=os.path.join(dataset_dir, image_id))
+
+    def load_mask(self, image_id):
+        """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+        info = self.image_info[image_id]
+        # Get mask directory from image path
+        mask_dir = os.path.join(os.path.dirname(os.path.dirname(info['path'])), "masks")
+
+        # Read mask files from .png image
+        mask = []
+        m = skimage.io.imread(os.path.join(mask_dir, image_id)).astype(np.bool)
+        mask.append(m)
+        
+        mask = np.stack(mask, axis=-1)
+        # Return mask, and array of class IDs of each instance. Since we have
+        # one class ID, we return an array of ones
+        return mask, np.ones([mask.shape[-1]], dtype=np.int32)
+
+    def image_reference(self, image_id):
+        """Return the path of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "medicare":
+            return info["id"]
+        else:
+            super(self.__class__, self).image_reference(image_id)
